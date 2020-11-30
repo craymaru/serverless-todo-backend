dc671d770824935fa4ada640a3993817e3fc6af1
Cray <craymaru@gmail.com>
2020-11-30 11:28:00 +0900

    mock の DynamoDB を操作するクラスを修正
    ddb.table.put() のように呼びたかったため、テーブルにもクラス持つよう修正

--- tests/mock/dynamo_db.py
+++ tests/mock/dynamo_db.py
@@ -5,38 +5,35 @@ class MockDynamoDB():
     """moto.mock_dynamodb2で作成されたDynamoDBを操作"""
 
     def __init__(self):
-        self._mock_dynamodb = None
-        self._mock_table = None
+        self.table = None
+        self._ddb = None
         self._last_responses = []
 
     def create_table(self, *, TableName, KeySchema, AttributeDefinitions, ProvisionedThroughput):
         """DynamoDBのテーブルを作成"""
-
-        _mock_dynamodb = boto3.resource("dynamodb")
-        self._mock_table = _mock_dynamodb.create_table(
-            TableName=TableName,
-            KeySchema=KeySchema,
-            AttributeDefinitions=AttributeDefinitions,
-            ProvisionedThroughput=ProvisionedThroughput,
+        self._ddb = boto3.resource("dynamodb")
+        self.table = MockDynamoDBTable(
+            self._ddb.create_table(
+                TableName=TableName,
+                KeySchema=KeySchema,
+                AttributeDefinitions=AttributeDefinitions,
+                ProvisionedThroughput=ProvisionedThroughput)
         )
         return self
 
-    def add_items(self, add_items, *args):
+    def set_table(self, table):
+        self.table = MockDynamoDBTable(table)
+
+
+class MockDynamoDBTable:
+
+    def __init__(self, table):
+        self._table = table
+
+    def put_items(self, items):
         """DynamoDBのテーブルに複数のアイテムを追加"""
 
-        self._responses = [
-            self._mock_table.put_item(Item=data) for data in add_items
+        self._last_responses = [
+            self._table.put_item(Item=data) for data in items
         ]
         return self
-
-    def mock_dynamodb(self):
-        """Return: mock_dynamodb"""
-        return self._mock_dynamodb
-
-    def mock_table(self):
-        """Return: mock_table"""
-        return self._mock_table
-
-    def last_responses(self):
-        """Return: last_responses"""
-        return self._last_responses
