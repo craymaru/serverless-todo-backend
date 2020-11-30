efc687c5854806367246448181aa7c43ad96ed3d
Cray <craymaru@gmail.com>
2020-11-30 19:46:45 +0900

    例外のエラーメッセージを修正

--- chalicelib/validates.py
+++ chalicelib/validates.py
@@ -14,73 +14,73 @@ class Validates:
     def subject(subject):
         if subject is None:
             raise BadRequestError(
-                f"Subject is None. (yours: {None})")
+                f"There is no subject (Your Request: {subject}) "
+                f"REQUIRED: subject is required.")
 
         subject_type = type(subject)
         if subject_type is not str:
             raise BadRequestError(
-                "Bad subject type. "
-                f"REQUIRED: {str} (yours: {subject_type})")
+                f"subject type (Your Request: {subject_type}) "
+                f"REQUIRED: {str}")
 
         subject_length = len(subject)
         if not Validates.SUBJECT_MIN_LEN <= subject_length <= Validates.SUBJECT_MAX_LEN:
             raise BadRequestError(
-                "Bad subject length. "
-                f"REQUIRED: greater than or equal to {Validates.SUBJECT_MIN_LEN}, "
-                f"less than or equal to {Validates.SUBJECT_MAX_LEN} "
-                f"(yours: {subject_length})")
+                f"subject length (Your Request: {subject_length}) "
+                f"REQUIRED: subject length is greater than or equal to {Validates.SUBJECT_MIN_LEN}, "
+                f"less than or equal to {Validates.SUBJECT_MAX_LEN}")
 
     @staticmethod
     def description(description):
         if description is None:
-            return None
+            return
 
         description_type = type(description)
         if description_type is not str:
             raise BadRequestError(
-                "Bad description type. "
-                f"REQUIRED: {str} (yours: {description_type})")
+                f"description type (Your Request: {description_type}) "
+                f"REQUIRED: {str}")
 
         description_length = len(description)
         if not description_length <= Validates.DESCRIPTION_MAX_LEN:
             raise BadRequestError(
-                "Bad subject length. "
-                f"REQUIRED: less than or equal to {Validates.DESCRIPTION_MAX_LEN} "
-                f"(yours: {description_length})")
+                f"subject length (Your Request: {description_length}) "
+                f"REQUIRED: less than or equal to {Validates.DESCRIPTION_MAX_LEN}")
 
     @staticmethod
     def state(state):
         if state is None:
             raise BadRequestError(
-                f"State is None. (yours: {state})")
+                f"there is no state. (Your Request: {None}) "
+                f"REQUIRED: state is required.")
 
         state_type = type(state)
         if state_type is not str:
             raise BadRequestError(
-                "Bad state type. "
-                f"REQUIRED: {str} (yours: {state_type})")
+                f"state type (Your Request: {state_type}) "
+                f"REQUIRED: {str}")
 
         if not state in Validates.STATE_ENUM:
             raise BadRequestError(
-                "Bad state. "
-                f"REQUIRED: strings of {', '.join(Validates.STATE_ENUM)} (yours: {state})")
+                f"state enum (Your Request: {state}) "
+                f"REQUIRED: {', '.join(Validates.STATE_ENUM)}")
 
     @staticmethod
     def username(username):
         if username is None:
             raise BadRequestError(
-                f"Username is None. (yours: {None})")
+                f"there is no username. (Your Request: {None}) "
+                f"REQUIRED: state is required.")
 
         username_type = type(username)
         if username_type is not str:
             raise BadRequestError(
-                "Bad username type. "
-                f"REQUIRED: {str} (yours: {username_type})")
+                f"username type (Your Request: {username_type}) "
+                f"REQUIRED: {str}")
 
         username_length = len(username)
         if not Validates.USERNAME_MIN_LEN <= username_length <= Validates.USERNAME_MAX_LEN:
             raise BadRequestError(
-                "Bad username length. "
+                f"username length (Your Request: {username_length}) "
                 f"REQUIRED: greater than or equal to {Validates.USERNAME_MIN_LEN}, "
-                f"less than {Validates.USERNAME_MAX_LEN} "
-                f"(yours: {username_length})")
+                f"less than {Validates.USERNAME_MAX_LEN}")
