3309017d5ee7bf486ecc64e81e102af1d9799a60
Cray <craymaru@gmail.com>
2020-12-04 01:34:50 +0900

    軽微な修正
    ddb_add_items.py
    main で実行するように修正

--- ddb_add_items.py
+++ ddb_add_items.py
@@ -1,7 +1,12 @@
 from tests.mock.dynamo_db import MockDynamoDB as DynamoDB
 from tests.testdata.ddb_items import TESTDATA_DDB_ITEMS
-from tests.mock.ddb_schema import MOCK_DDB_SCHEMA
 
-ddb = DynamoDB()
-ddb.set_table('serverless-todo-backend-f71fb8af-5dc0-4c8f-bdaf-e7e2d7124b7f')
-ddb.table.put_items(TESTDATA_DDB_ITEMS)
+
+def main():
+    ddb = DynamoDB()
+    ddb.set_table('serverless-todo-backend-0a4ca86a-8983-46fe-af1a-96a5a1e5bb12')
+    ddb.table.put_items(TESTDATA_DDB_ITEMS)
+
+
+if __name__ == '__main__':
+    main()
