6f9c34f555e0a2740e8020bcde65d80359607352
Cray <craymaru@gmail.com>
2020-12-05 07:36:33 +0900

    docstring の追加、更新

--- app.py
+++ app.py
@@ -20,7 +20,12 @@ authorizer = CognitoUserPoolAuthorizer(
 
 
 def get_authorized_username(current_request):
-    """ Auth時のトークンからcognito:usernameを取得"""
+    """トークンから cognito:username を取得
+    
+    Return:
+        str: cognito:username を返します
+        None: cognito:username がない場合 None を返します
+    """
     try:
         username = current_request.context['authorizer']['claims']["cognito:username"]
     except KeyError:
@@ -29,29 +34,30 @@ def get_authorized_username(current_request):
 
 
 def get_app_db():
-    """ TodoのDynamoDBテーブルを取得
+    """DynamoDBTodo インスタンスを取得します
 
-    Todo の DynamoDB テーブルを取得し、グローバル変数 _DB に格納します
+    Todo の DynamoDB テーブルを取得し、グローバル変数 _DB に格納します。
     このとき環境変数 APP_TABLE_NAME を参照し接続先のテーブル名を決定するため、
-    デプロイのステージごとに DynamoDB テーブルを変更することができます
-    環境変数の定義は .chalice/config.json を参照してください
+    デプロイのステージごとに DynamoDB テーブルを変更することができます。
+    環境変数の定義は .chalice/config.json を参照してください。
 
     envs:
-        APP_TABLE_NAME: 接続するDynamoDB上のテーブル名
         DYNAMO_DB_ENDPOINT:
-            接続するDynamoDBのエンドポイント
+            接続する DynamoDB のエンドポイント
             この環境変数が存在しない場合は、「呼び出し環境のcredentialsと同一の
-            AWSアカウント/リージョンに属するDynamoDB」にアクセスします
+            AWSアカウント/リージョンに属するDynamoDB」がエンドポイントとなります。
+        APP_TABLE_NAME: 接続する DynamoDB のテーブル名
 
     Return:
-        DynamoDBTodo(class):
-            DynamoDBへのアクセス情報を内包したDynamoDBTodoインスタンスを返します
+        DynamoDBTodo(instance):
+            DynamoDB のエンドポイントとテーブルへのアクセスを内包した DynamoDBTodo インスタンスを返します
 
     Examples:
-        get_app_db().list_items()
-        get_app_db().add_item()
-        get_app_db().update_item()
-        get_app_db().delete_item()
+        get_app_db().list_items()   username と query に基づいて Todo を取得します
+        get_app_db().get_item()    特定の Todo を取得します
+        get_app_db().add_item()    Todo を新規登録します
+        get_app_db().update_item() Todo を更新します
+        get_app_db().delete_item() Todo を削除します
 
     """
     global _DB
@@ -66,24 +72,27 @@ def get_app_db():
 
 @app.route('/todos', methods=['GET'], cors=True, authorizer=authorizer)
 def get_todos():
-    """ Todoのリストを取得
+    """クエリに基づき、Todo のリストを取得する DynamoDBTodo.list_items をコールします
 
-    app.current_request.query_params に格納されたURLクエリから、
-    DynamoDB を参照し Todo のリストを取得します
-    この時、Amazon Cognito の Auth 情報を参照し、
-    username ごとのリストを取得します
+    リクエストに含まれる Amazon Cognito の トークンを参照し、
+    username ごとに DynamoDB から取得し、 Todo オブジェクトのリストを返します。
+
+    このとき検索クエリが指定されている場合は、
+    Todo の subject, description キーの値に、検索クエリが含まれているものを取得します。
+
+    取得のため、DynamoDBTodo.list_items をコールします。
 
     Query Params:
-        ?q, ?s, ?search (str): Todo検索用のパラメータです
-        上記の3つのparamsは同一の検索クエリです
-        このURLクエリは Todo の subject, description を検索対象とします
+        ?q, ?s, ?search (str): 検索クエリ
+        上記の 3 つの params はすべて検索クエリです。
+        先頭から順に検索クエリの存在を確認し、見つかった場合は残りの検索クエリは無視します。
+    
+        クエリの指定のサンプル
+        `$ http POST <ENDPOINT_URI>/todos?q=<QUERY>`
+        (* 上記のコマンドは httpie パッケージを使用しています)
 
     Return:
-        list: Todoオブジェクトのリストを返します
-
-    Example:
-        http POST <ENDPOINT_URI>/todos?q=<QUERY>
-        (* 上記のコマンドは httpie パッケージを使用しています)
+        list: Todo オブジェクトのリストを返します
 
     """
     username = get_authorized_username(app.current_request)
@@ -99,18 +108,20 @@ def get_todos():
 
 @app.route('/todos', methods=['POST'], cors=True, authorizer=authorizer)
 def add_new_todo():
-    """ 新しいTodoを登録
+    """Todo を新規追加する DynamoDBTodo.add_item をコールします
 
-    app.current_request.json_body に格納されているパラメータを検証し、
-    DynamoDB にレコードを新規追加します
+    リクエストの json_body に含まれているパラメータをバリデーションし、
+    DynamoDB テーブルに特定のユーザーの Todo オブジェクトを追加するため、
+    DynamoDBTodo.add_item をコールします。
 
     Raises:
         BadRequestError:
-            json_bodyが存在しないケース
-            各種varidatesに失敗したケース
+            json_body が存在しないケースで例外が発生します。
+            バリデーションに失敗したケースで例外が発生します。
 
     Return:
-        str: 登録に成功したuidを返す
+        str: 登録に成功した Todo の uid を返します
+
     """
     body = app.current_request.json_body
     if body is None:
@@ -133,15 +144,16 @@ def add_new_todo():
 
 @app.route('/todos/{uid}', methods=['GET'], cors=True, authorizer=authorizer)
 def get_todo(uid):
-    """ 特定のTodoを取得
+    """特定の Todo オブジェクトを取得する DynamoDBTodo.get_item をコールします
 
-    DynamoDB から固有の uid を持つ Todo を取得します
+    DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを取得するため、
+    DynamoDBTodo.get_item をコールします。
 
     Args:
-        uid (str): 取得する uid を指定する
+        uid (str): 取得する uid を指定します
 
     Return:
-        dict: 特定のTodoを返す
+        dict: 特定の Todo オブジェクトを返します
 
     """
     username = get_authorized_username(app.current_request)
@@ -150,15 +162,16 @@ def get_todo(uid):
 
 @app.route('/todos/{uid}', methods=['DELETE'], cors=True, authorizer=authorizer)
 def delete_todo(uid):
-    """ 特定のTodoを削除
+    """特定の Todo オブジェクトを削除する DynamoDBTodo.delete_item をコールします
 
-    DynamoDB から固有の uid を持つ Todo を削除します
+    DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを削除するため、
+    DynamoDBTodo.delete_item をコールします。
 
     Args:
-        uid (str): 削除するTodoのuidを指定する
+        uid (str): 削除する Todo の uid を指定します
 
     Return:
-        uid: (str): 正常に削除されたTodoのuidを返す
+        uid: (str): 正常に削除された Todo の uid を返します
 
     """
     username = get_authorized_username(app.current_request)
@@ -167,19 +180,20 @@ def delete_todo(uid):
 
 @app.route('/todos/{uid}', methods=['PUT'], cors=True, authorizer=authorizer)
 def update_todo(uid):
-    """ 特定のTodoを更新
+    """特定の Todo オブジェクトを更新する DynamoDBTodo.update_item をコールします
 
-    DynamoDB から固有の uid を持つ Todo を取得し、
-    app.current_request.json_body に格納されている更新用パラメータを検証し、
-    DynamoDB のレコードを更新します
+    リクエストに含まれる json_body に格納されているパラメータをバリデーションします。
+
+    DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを更新するため、
+    DynamoDBTodo.update_item をコールします。
 
     Raises:
         BadRequestError:
-            json_bodyが存在しないケース
-            各種varidatesに失敗したケース
+            json_body が存在しないケースで例外が発生します。
+            バリデーションに失敗したケースで例外が発生します。
 
     Return:
-        uid: (str): 正常に更新されたTodoのuidを返す
+        uid: (str): 正常に更新された Todo の uid を返す
 
     """
     body = app.current_request.json_body
@@ -207,4 +221,5 @@ def update_todo(uid):
 
 @app.route('/', cors=True)
 def get_index():
+    """ウェルカムメッセージを返します"""
     return {'message': 'Welcome to serverless-todo api!'}
--- chalicelib/db.py
+++ chalicelib/db.py
@@ -22,16 +22,51 @@ def except_endpoint_connection_error(func):
 
 
 class DynamoDBTodo():
+    """DynamoDB テーブルに CRUD を行います
+
+    self.methods:
+        list_all_items : すべての Todo オブジェクトをスキャンします
+        list_items     : username と query に基づき、Todo オブジェクトのリストを取得します
+        add_item       : Todo オブジェクトを新規に追加します
+        get_item       : 特定の Todo オブジェクトを取得します
+        delete_item    : 特定の Todo オブジェクトを削除します
+        update_item    : 特定の Todo オブジェクトを更新します
+
+    constructer: 
+        Args: 
+            table_resource (boto3.resource.Table): 
+                リソースとエンドポイントとテーブルを指定済みの boto3.resource.Table インスタンス
+
+    self:
+        _table (boto3.resource.Table):
+            特定の DynamoDB テーブルを指定済みの boto3.resource.Table インスタンスを格納します。
+
+    """
+
     def __init__(self, table_resource):
         self._table = table_resource
 
     @except_endpoint_connection_error
     def list_all_items(self):
+        """すべての Todo オブジェクトをスキャンします"""
         response = self._table.scan()
         return response['Items']
 
     @except_endpoint_connection_error
     def list_items(self, query='', username=DEFAULT_USERNAME):
+        """username と query に基づき、Todo オブジェクトのリストを取得します
+
+        DynamoDB テーブルに登録されている特定のユーザーの Todo オブジェクトから、
+        subject、description いずれかに検索クエリを含む Todo オブジェクトのリストを取得します。
+
+        Args:
+            query (str): 検索クエリが渡ってきます クエリがない場合空文字を指定します
+            username (str): Todo のユーザー名が渡ってきます
+
+        Return:
+            list: Todo オブジェクトのリストを返します
+
+        """
         response = self._table.query(
             KeyConditionExpression=Key('username').eq(username),
             FilterExpression=(
@@ -43,6 +78,27 @@ class DynamoDBTodo():
 
     @except_endpoint_connection_error
     def add_item(self, subject, description='', username=DEFAULT_USERNAME):
+        """Todo オブジェクトを新規に追加します
+
+        DynamoDB テーブルに特定のユーザーの Todo オブジェクトを追加します。
+        uid は uuid4 で新規に生成します。*1
+        state は 初期値に 'unstarted' が設定されます。
+        subject, description, state, username キーの値にバリデーションを行います。
+
+        (*1 同一ユーザーの Todo オブジェクトにおける uuid4 の衝突は今回の仕様では考慮しません。)
+
+        Args:
+            subject (str): Todo のタイトルが渡ってきます
+            description (str): Todo の説明 が渡ってきます 指定がない場合空文字を指定します
+            username (str): Todo のユーザー名が渡ってきます
+
+        Raises:
+            BadRequestError: バリデーションに失敗したケースで例外が発生します
+
+        Return:
+            str: 登録に成功した Todo オブジェクトの uid を返します
+
+        """
         item = {
             'uid': str(uuid4()),
             'subject': subject,
@@ -60,6 +116,21 @@ class DynamoDBTodo():
 
     @except_endpoint_connection_error
     def get_item(self, uid, username=DEFAULT_USERNAME):
+        """特定の Todo オブジェクトを取得します
+
+        DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを取得します。
+
+        Args:
+            uid (str): 取得する uid が渡ってきます
+            username (str): 取得する Todo のユーザー名が渡ってきます
+
+        Raises:
+            NotFoundError: DynamoDB のレスポンスに Item キーがないケースで例外が発生します
+
+        Return:
+            dict: 特定の Todo オブジェクトを返します
+
+        """
         response = self._table.get_item(
             Key={
                 'username': username,
@@ -73,6 +144,21 @@ class DynamoDBTodo():
 
     @except_endpoint_connection_error
     def delete_item(self, uid, username=DEFAULT_USERNAME):
+        """特定の Todo オブジェクトを削除します
+
+        DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを削除します。
+
+        Args:
+            uid (str): 削除する uid
+            username (str): 削除する Todo のユーザー名
+
+        Raises:
+            NotFoundError: DynamoDB のレスポンスの Attributes の中に uid キーがないケースで例外が発生します
+
+        Return:
+            dict: 削除された Todo オブジェクトの uid を返します
+
+        """
         Validates.username(username)
         response = self._table.delete_item(
             Key={
@@ -89,6 +175,25 @@ class DynamoDBTodo():
     @except_endpoint_connection_error
     def update_item(self, uid, subject=None, description=None,
                     state=None, username=DEFAULT_USERNAME):
+        """特定の Todo オブジェクトを更新します
+
+        DynamoDB テーブルから特定のユーザー、uid の Todo オブジェクトを更新します。
+        subject, description, state, username キーの値にバリデーションを行います。
+
+        Args:
+            subject (str): Todo のタイトルが渡ってきます
+            description (str): Todo の説明が渡ってきます
+            username (str): Todo のユーザー名が渡ってきます
+            state (str): Todo のステータスが渡ってきます
+
+        Raises:
+            BadRequestError: バリデーションに失敗したケースで例外が発生します
+            NotFoundError: DynamoDB のレスポンスの Attributes の中に uid キーがないケースで例外が発生します
+
+        Return:
+            str: 更新に成功した Todo オブジェクトの uid を返します
+
+        """
         Validates.username(username)
         item = self.get_item(uid, username)
         if subject is not None:
